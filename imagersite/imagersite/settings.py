f0a2cd9f7ecae8ab9a6bb345a3880ad8d0046a18
Saki Fu <sakifu.86@gmail.com>
2015-07-30 22:20:10 -0700

    added email feature

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -114,3 +114,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
