6babef926b8a3538a53466ccd57542b2e1e76604
Saki Fu <sakifu.86@gmail.com>
2015-08-01 02:51:28 -0700

    fixed database

--- imagersite/imager_images/admin.py
+++ imagersite/imager_images/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Photo
-from .models import Album
+from .models import Photo, Album
 # Register your models here.
 
 admin.site.register(Photo)
--- imagersite/imager_profile/migrations/0001_initial.py
+++ imagersite/imager_profile/migrations/0001_initial.py
@@ -19,7 +19,7 @@ class Migration(migrations.Migration):
                 ('camera', models.CharField(help_text=b'What is model of your camera?', max_length=128)),
                 ('address', models.TextField()),
                 ('website_url', models.URLField()),
-                ('photography_type', models.CharField(help_text=b'What type of photography do you primaliry make?', max_length=64, choices=[(b'AB', b'Abstract'), (b'AN', b'Animal'), (b'AR', b'Artistic'), (b'BE', b'Beauty'), (b'FA', b'Fashion'), (b'LA', b'Landscape'), (b'NA', b'Nature'), (b'PE', b'People'), (b'TR', b'Travel'), (b'WE', b'Wedding')])),
+                ('photography_type', models.CharField(help_text=b'What type of photography do you primaliry make?', max_length=64, choices=[(b'Abstract', b'Abstract'), (b'Animal', b'Animal'), (b'Artistic', b'Artistic'), (b'Beauty', b'Beauty'), (b'Fashion', b'Fashion'), (b'Landscape', b'Landscape'), (b'Nature', b'Nature'), (b'People', b'People'), (b'Travel', b'Travel'), (b'Wedding', b'Wedding')])),
                 ('user', models.OneToOneField(related_name='profile', to=settings.AUTH_USER_MODEL)),
             ],
         ),
