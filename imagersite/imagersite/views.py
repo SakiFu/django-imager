19f2d86cb97975e50375136a38b8cb5cbdd4a5c9
Saki Fu <sakifu.86@gmail.com>
2015-08-05 10:42:20 -0700

    added profile_edit

--- imagersite/imagersite/views.py
+++ imagersite/imagersite/views.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView
-from imager_images.models import Photo, Album
+from django.views.generic import TemplateView
+from imager_images.models import Photo
 
 
 class IndexView(TemplateView):
@@ -12,21 +12,3 @@ class IndexView(TemplateView):
         photo = Photo.objects.all().filter(published='public').order_by('?').first()
         context['photo'] = photo
         return context
-
-class AlbumAddView(CreateView):
-    template_name = 'album_add.html'
-    model = Album
-    fields = ['title', 'description', 'published', 'photos']
-    success_url = '/images/library/'
-
-    def get_form(self):
-        form = super(AlbumAddView, self).get_form()
-        form.fields['photos'].queryset = Photo.objects.filter(
-            user=self.request.user
-        )
-        return form
-
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        form.save()
-        return super(AlbumAddView, self).form_valid(form)
