e12950250bc1a6eb8eb74e64eb1d3f9305e981fb
Saki Fu <sakifu.86@gmail.com>
2015-08-17 15:51:33 -0700

    edited

--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -30,14 +30,6 @@ class PhotoView(DetailView):
         return super(PhotoView, self).get_queryset(*args, **kwargs).filter(
             Q(user=self.request.user) | Q(published='Public'))
 
-    # def get_context_data(self, **kwargs):
-    #     context = super(DetailView, self).get_context_data(**kwargs)
-    #     if self.detect and len(self.object.faces.all()) == 0:
-    #         get_faces(self.object)
-
-    #     context['faces'] = self.object.faces.all()
-    #     return context
-
 
 class PhotoAddView(CreateView):
     template_name = 'photo_add.html'
