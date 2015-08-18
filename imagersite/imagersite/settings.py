b805907873edefbb853a3cedd1a82856b263a899
Saki Fu <sakifu.86@gmail.com>
2015-08-18 16:42:18 -0700

    changed database

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -84,18 +84,18 @@ WSGI_APPLICATION = 'imagersite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
-#     'default': dj_database_url.config(
-#         default='postgis://sakiukaji:@localhost:5432/django-imager'
-#     )
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
 # }
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default='postgis://sakiukaji:@localhost:5432/django-imager'
+     )
+}
 
 # # DATABASES = {
 # #     'default': {
