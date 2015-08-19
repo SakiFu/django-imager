b21ccb6b23661d2ba40446e65cbeefc8a03dbea1
Karen Wong <karen.wsywong@gmail.com>
2015-08-19 12:38:05 -0700

    add face model

--- imagersite/imager_images/admin.py
+++ imagersite/imager_images/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import Photo
-from .models import Album
+from .models import Album, Face
 # Register your models here.
 
 admin.site.register(Photo)
 admin.site.register(Album)
+admin.site.register(Face)
